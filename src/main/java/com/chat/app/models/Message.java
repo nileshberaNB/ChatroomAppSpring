b3737622d485c32759ff642cb91f38ddb91a2b76
nileshberaNB <nileshbera3012@gmail.com>
2022-07-16 12:57:14 +0530

    backend commit

---
+++ src/main/java/com/chat/app/models/Message.java
@@ -0,0 +1,21 @@
+package com.chat.app.models;
+
+public class Message {
+    private String name;
+    private String content;
+
+    public Message(String name, String content){
+        this.name=name;
+        this.content=content;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name=name;
+    }
+    
+
+}
