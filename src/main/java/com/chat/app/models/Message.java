34adff1bc843ab016c4fdcedcb0e6b5214b929cf
nileshberaNB <nileshbera3012@gmail.com>
2022-07-16 19:02:00 +0530

    second commit

--- src/main/java/com/chat/app/models/Message.java
+++ src/main/java/com/chat/app/models/Message.java
@@ -1,21 +1,29 @@
 package com.chat.app.models;
 
 public class Message {
+
     private String name;
     private String content;
 
-    public Message(String name, String content){
-        this.name=name;
-        this.content=content;
+    public Message(String name, String content) {
+        this.name = name;
+        this.content = content;
     }
 
-    public String getName(){
+
+    public String getName() {
         return name;
     }
 
-    public void setName(String name){
-        this.name=name;
+    public void setName(String name) {
+        this.name = name;
     }
-    
 
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
